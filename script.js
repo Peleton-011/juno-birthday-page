5ecdfde4a7c63cb584664fa210f534a8c6d3d68b
Nico <nicodiazan@gmail.com>
2022-10-04 19:56:26 +0200

    Add random vector generation

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 
-const genericStyle = "transition: all 2s ease; position: "absolute; width: 50px; height: 50px",
+const genericStyle = "transition: all 2s ease; position: absolute; width: 50px; height: 50px";
 
 function Vector (x, y) {
-    this.x = Number(x);
-    this.y = Number(y);
-    this.random = () => {
-        function negOnetoPosOne () {
-            return (Math.random() * 2) - 1;
-        }
-        this.x = negOnetoPosOne();
-        this.y = negOnetoPosOne();
-    }
+    this.x = Number(x) || (Math.random() * 2) - 1;
+    this.y = Number(y) || (Math.random() * 2) - 1;
 }
 
 function Sticker (path, vector, dist1, dist2) {
@@ -20,7 +13,7 @@ function Sticker (path, vector, dist1, dist2) {
 function setup () {
     const button = document.querySelector("#mainBut");
     button.addEventListener("click", (e) => {
-        switchState();
+        console.log(new Vector())
     });
 }
 
@@ -28,7 +21,7 @@ function createTestDiv () {
     const div = document.createElement("div");
     const body = document.querySelector("body");
     div.classList.add("test");
-    div.style.cssText = ;
+    div.style.cssText = "color: blue;";
     body.appendChild(div);
 }
 
