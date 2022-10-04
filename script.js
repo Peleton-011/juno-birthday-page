a8fd53b0bee9e60bd360d37c6d005b6ff79a0701
Nico <nicodiazan@gmail.com>
2022-10-04 20:40:40 +0200

    Add getLimit() function.
    it returns which side of the viewport a vector will collide with
    this will be used to determine whether a certain point along the vector
    is inside or outside the viewport to create a slide-in effect.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 
 const genericStyle = "transition: all 2s ease; position: absolute; width: 50px; height: 50px";
 
+const vw = window.innerWidth;
+const vh = window.innerHeight;
+
 function Vector (x, y) {
     this.x = Number(x) || (Math.random() * 2) - 1;
     this.y = Number(y) || (Math.random() * 2) - 1;
+}
+
+// Gets coordinates at which this vector intersects with the viewport
+function getLimit(thisVector, vh, vw) {
+    // Top: 0
+    // Right: 1
+    // Bottom: 2
+    // Left: 3
+    const ratio = thisVector.y * (vw / vh);
+    let result;
+    if ((thisVector.x < ratio) && (thisVector.x > thisVector.y * ratio)) {
+        (thisVector.y > 0) && (result = 0);
+        (thisVector.y < 0) && (result = 2);
+    }
+    (thisVector.x > 0) && (result = 1);
+    (thisVector.x < 0) && (result = 3);
+    return result;
 }
 
 function Sticker (path, vector, dist1, dist2) {
