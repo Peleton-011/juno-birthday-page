92165b05e2940540e59a435c1c37da82cacdb861
Nico <nicodiazan@gmail.com>
2022-10-05 19:12:29 +0200

    Add addToDOM() function to easily add elements to the DOM

--- script.js
+++ script.js
@@ -25,6 +25,25 @@ function switchState(willHide) {
         "transition: all 2s ease; position: absolute; left: 10vw;";
 }
 
+// Adds an element to the DOM of the tag "tag", as a child of the node which fits "selector", with the inline style "style"
+// You can add any other attribute as an array ["attributeName", "attributeValue"]
+
+function addToDOM(tag, selector, style) {
+    const target = document.querySelector(selector);
+    const element = document.createElement(tag);
+    if (style) {
+        element.style.cssText = style;
+    }
+    for (let i = 3; i < arguments.length; i++) {
+        if (arguments[i]) {
+            element.setAttribute(arguments[i][0], arguments[i][1]);
+        }
+    }
+    if (target) {
+        target.appendChild(element);
+    }
+}
+
 // Gets coordinates at which this vector intersects with the viewport
 function getLimit(thisVector, quadrilateral) {
     // Functions to find the limit depending on the side it intersects with first
@@ -66,6 +85,14 @@ function getRandDistance(max) {
     return Math.max(Math.random(), Math.random() + 0.15) * max;
 }
 
+// Calculates absolute coordinates based on vector and distance relative to the center
+function calcCoords(vec, dist, offset) {
+    // First calculate relative position
+    const rel = new Vector(vec.x * dist + offset.x, vec.y * dist + offset.y);
+    const abs = new Vector(rel.x + viewport.x / 2, rel.y + viewport.y / 2);
+    return abs;
+}
+
 function Vector(x, y) {
     this.x = Number(x) || Math.random() * 2 - 1;
     this.y = Number(y) || Math.random() * 2 - 1;
@@ -84,17 +111,23 @@ function Vector(x, y) {
 
 function Sticker(path, vector, offset) {
     this.path = path;
+
     this.vector = vector || new Vector();
     this.vector = this.vector.unitVector();
+
     this.limit = getLimit(this.vector, viewport);
+
     this.dist1 = getRandDistance(this.limit.length);
     this.dist2 = this.dist1 + this.limit.length;
+
     this.offset =
         offset ||
         new Vector(
             Math.random * (this.limit.length - this.dist1),
             Math.random * (this.limit.length - this.dist1)
         );
+
+    this.init = "CUM";
 }
 
 setup();
